7cc0cad164efef679cd04d9bc804a787998d2ee4
j3rry01v <jerinsr19@gmail.com>
2021-03-17 18:13:20 +0530

    added some tweaks

--- Semester_01/Python-Programming-Lab/Course-Outcome-3-(CO3)/02-Package-Area-Perimeter/main.py
+++ Semester_01/Python-Programming-Lab/Course-Outcome-3-(CO3)/02-Package-Area-Perimeter/main.py
@@ -16,7 +16,7 @@ from Graphics.ThreeDG import Sphere as sp
 # user input
 while True:
     print("\n❖ ❖ ❖ ❖ ❖ ❖ ❖ SELECT ANY TO PERFORM OPERATION  ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖\n")
-    option = int(input("\n 1. Rectangle \n 2.Circle \n 3. Cuboid \n 4.Sphere\n"))
+    option = int(input("\n 1. Rectangle \n 2.Circle \n 3.Cuboid \n 4.Sphere\n"))
     print("❖ ❖ ❖ ❖ ❖ ❖ ❖ SELECT ANY TO PERFORM OPERATION  ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖")
     if option == 1:
         print("❖ ❖ ❖ ❖ ❖ ❖ ❖ RECTANGLE OPERATIONS  ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖")
@@ -26,8 +26,8 @@ while True:
             breadth = int(input("Enter the Breadth Rectangle\t"))
             print("Area is ", r.area(length, breadth))
         elif rectOption == 2:
-            length = int(input("Enter the length of Rectangle"))
-            breadth = int(input("Enter the Breadth of Rectange"))
+            length = int(input("Enter the length of Rectangle \t"))
+            breadth = int(input("Enter the Breadth of Rectange\t"))
             print("Perimeter is ", r.perimeter(length, breadth))
         else:
             print("Invalid Option !")
@@ -35,10 +35,10 @@ while True:
         print("❖ ❖ ❖ ❖ ❖ ❖ ❖ CIRCLE OPERATIONS  ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖")
         circOption = int(input("\n 1.Area \n 2.Perimeter \n"))
         if circOption == 1:
-            r = int(input("Enter the Radius"))
+            r = int(input("Enter the Radius \t"))
             print("Area of Circle with radius ", r, "is ", c.area(r))
         elif circOption == 2:
-            r = int(input("Enter the Radius"))
+            r = int(input("Enter the Radius \t"))
             print("Perimeter of the circle with radius ", r, "is", c.perimeter(r))
         else:
             print("Invalid Option !")
@@ -46,19 +46,19 @@ while True:
         print("❖ ❖ ❖ ❖ ❖ ❖ ❖ CUBOID OPERATIONS  ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖")
         qubOption = int(input("\n 1.Area \n 2.Perimeter \n 3.Volume \n"))
         if qubOption == 1:
-            length = int(input("Enter the length of Cuboid"))
-            width = int(input("Enter the Height of Cuboid"))
-            height = int(input("Enter the Breadth of Cuboid"))
+            length = int(input("Enter the length of Cuboid \t"))
+            width = int(input("Enter the Height of Cuboid\t"))
+            height = int(input("Enter the Breadth of Cuboid\t"))
             print("Surface Area of Cuboid is ", cd.area(length, width, height))
         elif qubOption == 2:
-            length = int(input("Enter the length of Cuboid"))
-            width = int(input("Enter the Height of Cuboid"))
-            height = int(input("Enter the Breadth of Cuboid"))
+            length = int(input("Enter the length of Cuboid\t"))
+            width = int(input("Enter the Height of Cuboid\t"))
+            height = int(input("Enter the Breadth of Cuboid\t"))
             print("Perimeter of Cuboid is ", cd.perimeter(length, width, height))
         elif qubOption == 3:
-            length = int(input("Enter the length of Cuboid"))
-            width = int(input("Enter the Height of Cuboid"))
-            height = int(input("Enter the Breadth of Cuboid"))
+            length = int(input("Enter the length of Cuboid\t"))
+            width = int(input("Enter the Height of Cuboid\t"))
+            height = int(input("Enter the Breadth of Cuboid\t"))
             print("Volume of Cuboid is ", cd.volume(length, width, height))
         else:
             print("Invalid Option !")
@@ -66,13 +66,13 @@ while True:
         print("❖ ❖ ❖ ❖ ❖ ❖ ❖ SPHERE OPERATIONS  ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖ ❖")
         sphOption = int(input("\n 1.Area \n 2.Perimeter \n 3.Volume \n"))
         if sphOption == 1:
-            r = int(input("Enter the Radius"))
+            r = int(input("Enter the Radius\t"))
             print("Area of Sphere with radius ", r, "is", sp.area(r))
         elif sphOption == 2:
-            r = int(input("Enter the Radius"))
+            r = int(input("Enter the Radius\t"))
             print("Perimeter of Sphere with radius ", r, "is", sp.perimeter(r))
         elif sphOption == 3:
-            r = int(input("Enter the Radius"))
+            r = int(input("Enter the Radius\t"))
             print("Volume of Sphere with radius ", r, "is",sp.volume(r))
     else:
         print("Invalid Option !")

f00cdc67536eb4cc61ddb69afa73f0da66cd1e66
j3rry01v <jerinsr19@gmail.com>
2021-03-17 12:41:33 +0530

    co3

---
+++ Semester_01/Python-Programming-Lab/Course-Outcome-3-(CO3)/02-Package-Area-Perimeter/main.py
@@ -0,0 +1,7 @@
+from Graphics import Circle as c
+from Graphics import Rectangle as r
+
+print(c.area(4))
+print(c.perimeter(4))
+print(r.area(3, 6))
+print(r.area(5, 9))
---
+++ Semester_01/Python-Programming-Lab/Course-Outcome-3-(CO3)/temp/02-Package-Area-Perimeter/Graphics/Circle.py
@@ -0,0 +1,9 @@
+import math
+
+
+def area(r):
+    return math.pi * math.pow(r, 2)
+
+
+def perimeter(r):
+    return 2 * math.pi * r

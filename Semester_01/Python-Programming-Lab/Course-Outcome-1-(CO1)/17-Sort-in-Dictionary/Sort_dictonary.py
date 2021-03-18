dc07a3028ae8820625d4a8d982fcf2094ddb8309
j3rry01v <jerinsr19@gmail.com>
2021-03-18 18:22:08 +0530

    updated sort in dictionary with user input
    dictonary user input

--- Semester_01/Python-Programming-Lab/Course-Outcome-1-(CO1)/17-Sort-in-Dictionary/Sort_dictonary.py
+++ Semester_01/Python-Programming-Lab/Course-Outcome-1-(CO1)/17-Sort-in-Dictionary/Sort_dictonary.py
@@ -7,16 +7,22 @@
 # dictItem[name] = values
 #  ""
 #
-dictItem = {'apple': 40, 'orange': 2, 'banana': 1, 'lemon': 3}
+dict1 = {}
+print(type(dict1))
+limit = int(input("Enter  the limit"))
+for i in range(limit) :
+    dict1.update({input("Enter the key") : input("Enter the value")})
+# dictItem = {'apple': 40, 'orange': 2, 'banana': 1, 'lemon': 3}
 
-l = list(dictItem.items())  # dict to list conversion
+l = list(dict1.items())  # dict to list conversion
 l.sort()
 print("\n Ascending order is", l)  # sorted list
 
-l = list(dictItem.items())
+l = list(dict1.items())
 l.sort(reverse=True)  # sorting in reverse order
 print("\nDescending order is", l)
 
-dict = dict(l)  # list to dict
+dict  = dict(l)  # list to dict
 
 print("\nDictionary", dict)
+print(("dict is" , dict1))

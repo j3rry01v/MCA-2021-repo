a1348c71bca1c804f352f9cb25aeb19cb455dab9
j3rry01v <jerinsr19@gmail.com>
2021-05-26 12:50:53 +0530

    array loop

---
+++ Semester-02/Object-Oriented-Programming-Lab/First-Fifteen/12-Array-With-Loop/arrayloop.java
@@ -0,0 +1,14 @@
+
+
+public class arrayloop {
+    public static void main (String args [])
+    {
+   
+   int  [] data={12,4,5,2};
+   for(int i=0; i<3; i++)
+   {
+    System.out.print(data[i] + " ");
+    }
+    
+}
+}

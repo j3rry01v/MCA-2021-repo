83b9874c6d00c12a953176c316a29de0bc0ec782
j3rry01v <jerinsr19@gmail.com>
2021-05-26 11:46:44 +0530

    circle area perimeter

---
+++ Semester-02/Object-Oriented-Programming-Lab/First-Fifteen/04-Area-Perimeter/areaperimeter.java
@@ -0,0 +1,15 @@
+
+public class areaperimeter {
+    public static void main(String args []) {
+        
+
+        Double radius=7.5;
+        Double pi =3.14 ;
+        System.out.println("\nTest Data:");
+        System.out.println("Radius = "+radius);
+        System.out.println("Expected Output");
+        System.out.println("Perimeter is = " + (2*pi*radius));
+        System.out.println("Area is = " + (pi*radius*radius));
+    }
+    
+}

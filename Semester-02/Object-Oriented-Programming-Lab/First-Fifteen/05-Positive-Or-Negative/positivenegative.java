c921861699575f9bd8707e65d10c2b35630d8eae
j3rry01v <jerinsr19@gmail.com>
2021-05-26 11:51:46 +0530

    positive negative

---
+++ Semester-02/Object-Oriented-Programming-Lab/First-Fifteen/05-Positive-Or-Negative/positivenegative.java
@@ -0,0 +1,13 @@
+
+
+public class positivenegative {
+    public static void main(String[] args) {
+        int num =10 ;
+        if(num > 0)
+            System.out.println("The Number is positive");
+        else 
+            System.out.println("The Number is Negative");
+
+    }
+    
+}

18065f88410128e0adf9515f36c1dd7fdee9daae
j3rry01v <jerinsr19@gmail.com>
2021-05-26 11:56:19 +0530

    greatest of three

---
+++ Semester-02/Object-Oriented-Programming-Lab/First-Fifteen/06-Greatest-Among-Three/greatestofthree.java
@@ -0,0 +1,18 @@
+
+
+public class greatestofthree {
+    
+    public static void main(String[] args) {
+
+        int num1 = 10, num2 = 20, num3 = 30;
+  
+        if( num1 >= num2 && num1 >= num3)
+            System.out.println(num1+" is  Greatest");
+  
+        else if (num2 >= num1 && num2 >= num3)
+            System.out.println(num2+" is Greatest");
+  
+        else
+            System.out.println(num3+" is Greatest");
+    }
+}

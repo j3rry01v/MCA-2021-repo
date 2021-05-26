5064acae620bd54f274b2daabe6ca17ff36b425a
j3rry01v <jerinsr19@gmail.com>
2021-05-26 12:46:11 +0530

    accessig array outout error exist

---
+++ Semester-02/Object-Oriented-Programming-Lab/First-Fifteen/11-Access-Array-Elements/arrayelements.java
@@ -0,0 +1,15 @@
+
+
+public class arrayelements {
+    public static void main (String args [])
+     {
+    
+    int  [] data={12,4,5,2};
+    
+        System.out.printf("First Element :",data[0]);
+        System.out.printf("Second Element :",data[1]);
+        System.out.printf("Third Element :",data[2]);
+        System.out.printf("Fourth Element :",data[3]);
+        // output error in exist - fixing soon
+     }   
+}

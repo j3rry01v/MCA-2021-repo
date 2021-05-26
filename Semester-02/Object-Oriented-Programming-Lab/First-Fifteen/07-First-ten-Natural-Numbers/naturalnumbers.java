f74e5d1b36a83ce9bed12e36f8caa1673e2bbe51
j3rry01v <jerinsr19@gmail.com>
2021-05-26 11:59:40 +0530

    natural numbers

---
+++ Semester-02/Object-Oriented-Programming-Lab/First-Fifteen/07-First-ten-Natural-Numbers/naturalnumbers.java
@@ -0,0 +1,15 @@
+
+
+public class naturalnumbers {
+    public static void main(String[] args)
+    {     
+    int i;
+	System.out.println ("First ten natural numbers are...\n");
+	for (i=1;i<=10;i++)
+	{      
+		System.out.println (i);
+	}
+System.out.println ("\n");
+    }
+    
+}

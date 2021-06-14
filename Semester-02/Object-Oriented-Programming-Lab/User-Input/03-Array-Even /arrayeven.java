0225999d10bd89fd0b2cbb01ddd0883b86fedd3b
j3rry01v <jerinsr19@gmail.com>
2021-06-14 14:02:19 +0530

    even elements

---
+++ Semester-02/Object-Oriented-Programming-Lab/User-Input/03-Array-Even /arrayeven.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class arrayeven
+{
+    public static void main(String[] args)
+     {
+        int element_count;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number of elements  for the array ");
+        element_count = scan.nextInt();
+        int[] array = new int[10];
+        System.out.println("Now enter array elements ");
+        for (int i = 0; i < element_count; i++) 
+        {
+            array[i] = scan.nextInt();
+        } 
+        System.out.println("Elements of array are ");
+        for (int i = 0; i < element_count; i++) 
+        {
+            System.out.println(array[i]);
+        }
+        System.out.println("Even Elements of array are ");
+        for (int i = 0; i < element_count; i++) 
+        {
+            if(array[i]%2==0)
+            {
+            System.out.println(array[i]);
+            }
+            else
+            System.out.println(" ");
+
+        }
+    }
+}

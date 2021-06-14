5e8bc1c675121938f1eb4f5d69197d17fa685d40
j3rry01v <jerinsr19@gmail.com>
2021-06-14 14:42:02 +0530

    array count updated

---
+++ Semester-02/Object-Oriented-Programming-Lab/User-Input/07-Array-Number-of-Elements/arrayelementcount.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class arrayelementcount 
+{
+    public static void main(String[] args)
+     {
+        int element_count;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number of elements  for the array ");
+        element_count = scan.nextInt();
+        int[] array = new int[element_count];
+        System.out.println("Now enter array elements ");
+        for (int i = 0; i < element_count; i++) 
+        {
+            array[i] = scan.nextInt();
+        } 
+        System.out.println("Elements of array are ");
+        for (int i = 0; i < element_count; i++) 
+        {
+            System.out.println(array[i]);
+        }
+        // int counter = 0;
+        // for (int i = 0; i < element_count; i ++)
+        // {
+        //     {
+        //       if (array[i] != null)}
+        //          counter ++;
+        //     }
+        System.out.println("Number of Elements in array are " +element_count);
+
+    }
+}

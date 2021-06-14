e59680e11ffe437698ad4f0177ed2efeea098494
j3rry01v <jerinsr19@gmail.com>
2021-06-14 14:32:24 +0530

    array smallest element

---
+++ Semester-02/Object-Oriented-Programming-Lab/User-Input/06-Array-Smallest-Element/arraysmallest.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class arraysmallest
+{
+    public static void main(String[] args)
+     {
+        int element_count;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number of elements  for the array ");
+        element_count = scan.nextInt();
+        int[] array = new int[10];
+        System.out.println("Now enter array elements ");
+        for (int i = 0; i < element_count; i++) 
+        {
+            array[i] = scan.nextInt();
+        } 
+        System.out.println("Elements of array are ");
+        for (int i = 0; i < element_count; i++) 
+        {
+            System.out.println(array[i]);
+        }
+        int smallest;
+        smallest = array[0];
+	   int j =0;
+       for(j=0; j<element_count; j++)
+       {
+           if(smallest > array[j])
+           {
+               smallest = array[j];
+           }
+           
+       }
+        System.out.println("Smallest element in array is " +smallest);
+    }
+}

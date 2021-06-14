e7cbc468c315cea92d1198f90d61e2b517e0c8c7
j3rry01v <jerinsr19@gmail.com>
2021-06-14 14:05:27 +0530

    array odd elements

---
+++ Semester-02/Object-Oriented-Programming-Lab/User-Input/04-Array-Odd/arrayodd.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class arrayodd
+{
+    public static void main(String[] args)
+     {
+        int element_count;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number of elements  for the array ");
+        element_count = scan.nextInt();
+        int[] array = new int[10];
+        System.out.println("Now enter array elements ");
+        for (int i = 0; i < element_count; i++) 
+        {
+            array[i] = scan.nextInt();
+        } 
+        System.out.println("Elements of array are ");
+        for (int i = 0; i < element_count; i++) 
+        {
+            System.out.println(array[i]);
+        }
+        System.out.println("Odd Elements of array are ");
+        for (int i = 0; i < element_count; i++) 
+        {
+            if(array[i]%2==0)
+            {
+            System.out.println(" ");
+            }
+            else
+            System.out.println(array[i]);
+
+        }
+    }
+}

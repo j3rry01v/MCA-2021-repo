c26de6ee6559521e52603b706583f29a6bb58a74
j3rry01v <jerinsr19@gmail.com>
2021-06-24 02:56:19 +0530

    string manupulation : not complete

--- Semester-02/Object-Oriented-Programming-Lab/Course-Outcome-2-(CO2)/03-Perform-String-Manipulations/Readme.md
+++ Semester-02/Object-Oriented-Programming-Lab/Course-Outcome-2-(CO2)/03-Perform-String-Manipulations/Readme.md
@@ -1,18 +1,17 @@
-# Course Outcome 1
-## Question 5
-Create CPU with attribute price. Create inner class Processor (no. of cores, manufacturer) and static nested class RAM (memory, manufacturer). Create an object of CPU and print information of Processor and RAM.
-
+# Course Outcome 2
+## Question 3
+Perform string manipulations
 
 
 ##### Compile the code
 
 
 ```sh
-javac cpu.java
+javac stringManupulation.java
 ```
 
 ### Running the code:
 
 ```sh
-java cpu
+java stringManupulation
 ```
---
+++ Semester-02/Object-Oriented-Programming-Lab/Course-Outcome-2-(CO2)/03-Perform-String-Manipulations/stringManupulation.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+class stringManipulation
+{
+    String data;
+
+    stringManipulation(String data)
+    {
+        this.data=data;
+    }
+
+   void stringToUpperCase()
+   {
+    System.out.println("\n Converted to UPPERCASE ");
+
+    System.out.println(data.toUpperCase());
+   }
+
+   void stringToLowerCase()
+   {
+    System.out.println("\n Converted to lowercase ");
+
+    System.out.println(data.toLowerCase());
+   }
+
+   void stringReplace (string tempchar , string replace_text)
+   {
+       System.out.println("Which one to replace");
+
+       tempchar = scan.nextLine();
+
+       System.out.println("Replace with ?  ");
+
+       replace_text =scan.nextLine();
+
+       System.out.println("Replaced...!");
+
+       System.out.println(data.replace(tempchar, replace_text));
+
+   }
+   void stringReverse ()
+   {
+
+    System.out.println("\n String reversed ");
+
+    System.out.println(data.reverse());
+   }
+}
+
+public class StringManipulations 
+{
+    public static void main(String[] args) 
+    {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a string ");
+        data =scan.nextLine();
+
+		while(true)
+        {
+			System.out.println("1.To Uppercase");
+
+			System.out.println("2.To Lowercase");
+
+			System.out.println("3.Replace Characters");
+
+			System.out.println("4.Reverse");
+
+			System.out.println("Enter your choice::");
+
+			int choice = scan.nextInt();
+
+			switch(choice)
+            {
+			case 1 :stringToUpperCase();
+			break;
+			case 2 :stringToLowerCase();
+			break;
+			case 3 :stringReplace(); 
+			break;
+            case 4 :stringReverse();
+			default: System.out.println("Invalid option");
+			}
+		}
+
+	}
+}
